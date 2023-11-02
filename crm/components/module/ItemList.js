02b6c1aecdfaa3916040057cdd657ffeb9d09149
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-02 17:26:04 +0330

    some changes in itemlist added

--- crm/components/module/ItemList.js
+++ crm/components/module/ItemList.js
@@ -27,33 +27,12 @@ const ItemList = ({ form, setForm }) => {
     <div className="item-list">
       <p>Purchased Products</p>
       {products.map((product, index) => (
-        <div key={index} className="form-input__list">
-          <FormInput
-            name="name"
-            type="text"
-            label="Product Name"
-            value={product.name}
-            onChange={(e) => changeHandler(e, index)}
-          />
-          <div>
-            <FormInput
-              name="price"
-              type="text"
-              label="Price"
-              value={product.price}
-              onChange={(e) => changeHandler(e, index)}
-            />
-
-            <FormInput
-              name="qty"
-              type="number"
-              label="Qty"
-              value={product.qty}
-              onChange={(e) => changeHandler(e, index)}
-            />
-          </div>
-          <button onClick={() => deleteHandler(index)}>Remove</button>
-        </div>
+        <ProductItem
+          key={index}
+          product={product}
+          changeHandler={(e) => changeHandler(e, index)}
+          deleteHandler={() => deleteHandler(index)}
+        />
       ))}
 
       <button onClick={addHandler}>Add Item</button>
@@ -62,3 +41,35 @@ const ItemList = ({ form, setForm }) => {
 };
 
 export default ItemList;
+
+function ProductItem({ product, changeHandler, deleteHandler }) {
+  return (
+    <div className="form-input__list">
+      <FormInput
+        name="name"
+        type="text"
+        label="Product Name"
+        value={product.name}
+        onChange={changeHandler}
+      />
+      <div>
+        <FormInput
+          name="price"
+          type="text"
+          label="Price"
+          value={product.price}
+          onChange={changeHandler}
+        />
+
+        <FormInput
+          name="qty"
+          type="number"
+          label="Qty"
+          value={product.qty}
+          onChange={changeHandler}
+        />
+      </div>
+      <button onClick={deleteHandler}>Remove</button>
+    </div>
+  );
+}
